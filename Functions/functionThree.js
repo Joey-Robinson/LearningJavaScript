9dd69c48bf035221de216a262e9f0cbe41cbe1cf
elamahpla <theamazingness@gmail.com>
2018-05-30 10:48:27 -0500

    .

--- Functions/functionThree.js
+++ Functions/functionThree.js
@@ -12,8 +12,16 @@
 
 // console.log(mathFunction(2, 2));
 // More Defaults
-const getScoreText = (name = 'Anonymous', score = 100) => {
-  console.log(name, score);
-};
+// const getScoreText = (name = 'Anonymous', score = 100) => {
+//   return `Name: ${name}, Score: ${score}`
+// };
 // If no value is given in the callback, default will be used
-getScoreText('Bob', 10);
+// console.log(getScoreText());
+
+// Challenge
+// 2 Arguments: total, tipPercent: .2 
+const getTip = (total, tipPercent = .2) => {
+  return total * tipPercent
+};
+
+console.log(getTip(100));
