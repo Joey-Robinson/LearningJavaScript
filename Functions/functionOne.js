856de3b290f8516e32e6ea5593f5eb4e5bf88d79
joeyrobinson <joeyrobinsondev@gmail.com>
2018-05-30 10:07:33 -0500

    .

---
+++ Functions/functionOne.js
@@ -0,0 +1,37 @@
+// Function - input (argument(s)), code, output (return value)
+
+// const greetUser = () => {
+//   console.log(`Welcome User`);
+// };
+
+// greetUser();
+
+// const numberSquared = (num) => {
+  // console.log(num * num);
+//   const result = num * num;
+//   return result
+// };
+
+// const value = numberSquared(9);
+// console.log(value);
+// numberSquared(3);
+
+// Challenge
+
+// const tempConversion = (fahrenheit, kelvin) => {
+//   return (fahrenheit - 32) * 5 / 9;
+//   return (fahrenheit + 459.67) * 5 / 9;
+// };
+
+// const usTemp = tempConversion(90);
+// const kel = tempConversion(0, 20);
+// console.log(usTemp);
+// console.log(kel);
+
+// let convertTemp = function(temp) {
+//   let celsius = (temp - 32) * 5 / 9;
+//   return celsius
+// };
+
+// let tempOne = convertTemp(32);
+// console.log(tempOne);
