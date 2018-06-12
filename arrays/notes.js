ad130c103a9faca57fc72f4bdfa5d00844013bee
elamahpla <theamazingness@gmail.com>
2018-06-12 10:16:59 -0500

    Stuff

---
+++ arrays/notes.js
@@ -0,0 +1,4 @@
+const notes = ['Note One', 'Note Two', 'Note Three'];
+
+console.log(notes.length);
+console.log(notes[notes.length - 1]);
---
+++ arrays/todo.js
@@ -0,0 +1,9 @@
+const todoList = [
+  'Get Coffee',
+  'Finish Layout',
+  'Get Coffee',
+  'Go For Walk',
+  'Take Shower'
+];
+
+console.log(`You have at least ${todoList.length} things to get done today. ${todoList[0]} and ${todoList[todoList.length - 1]} are pretty important`);
