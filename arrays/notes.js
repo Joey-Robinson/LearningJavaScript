c1e395c94b9b97fb0805a1b208f057b555492aa7
elamahpla <theamazingness@gmail.com>
2018-06-13 14:12:47 -0500

    Super slow day, not a lot to be done.

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,4 +1,63 @@
-const notes = ['Note One', 'Note Two', 'Note Three'];
+// const notes = ['Note One', 'Note Two', 'Note Three'];
+const notes = [{
+  title: 'My next trip',
+  body: 'I would like to go to Spain'
+}, {
+  title: 'Habbits to work on',
+  body: 'Exercise. Eating Healthier.'
+}, {
+  title: 'New Monitor',
+  bod: 'Get another monitor'
+}];
 
-console.log(notes.length);
-console.log(notes[notes.length - 1]);
+// const findNote = (notes, noteTitle) => {
+//   const index = notes.findIndex((note, index) => {
+//     return note.title.toLowerCase() === noteTitle.toLowerCase();
+//   });
+//   return notes[index];
+// };
+
+const findNote = (notes, noteTitle) => {
+  return notes.find((note, index) => {
+    return note.title.toLowerCase() === noteTitle.toLowerCase();
+  });
+};
+
+console.log(findNote(notes, 'MY NEXT TRIP'));
+// notes.findIndex((note, index) => {
+//   console.log(note, index);
+// });
+// console.log(notes.indexOf('Note One'));
+
+// for(let count = 2; count >= 0; count--) {
+//   console.log(count);
+// }
+
+// for(let count = notes.length - 1; count >= 0; count--) {
+//   console.log(notes[count]);
+// }
+
+// for(let value of notes) {
+//   value += ' Test';
+//   console.log(value);
+// }
+
+// The parameter (item) here is the notes variable. So when I console.log() it, it will print the array as 3 individual items
+// The parameter (index) here is the placement of each item in the array. Using console.log() will print Note One 0, for example, and continue on until done.
+// notes.forEach((item, index) => {
+//   console.log(item, index);
+// });
+// console.log(notes);
+// notes.splice(0, 0, 'Note Zero');
+// First argument is where, or the index.
+// Second argument is the number of items you want to remove. (Or add)
+// Third argument is optional. It allows you to inject an item depending on where your first and second arguments are located at. 
+// Optionally, you can use the second argument to remove that location (eg: 1, 1, 'Something') would remove
+// 'Note Two' and replace it with 'Note Zero'
+// notes[2] = 'New Note Three';
+// notes.unshift('Notes Zero');
+// console.log(notes.length);
+// notes.push('Note Four');
+// console.log(notes[notes.length - 1]);
+// notes.pop();
+// console.log(notes.shift());
--- arrays/todo.js
+++ arrays/todo.js
@@ -1,9 +1,74 @@
-const todoList = [
-  'Get Coffee',
-  'Finish Layout',
-  'Get Coffee',
-  'Go For Walk',
-  'Take Shower'
-];
+// const todoList = [
+//   'Get Coffee',
+//   'Finish Layout',
+//   'Get Water',
+//   'Go For Walk',
+//   'Take Shower'
+// ];
 
-console.log(`You have at least ${todoList.length} things to get done today. ${todoList[0]} and ${todoList[todoList.length - 1]} are pretty important`);
+const todos = [{
+  body: 'Get Coffee',
+  completed: true
+}, {
+  body: 'Finish Layout',
+  completed: true
+}, {
+  body: 'Get Water',
+  completed: true
+}, {
+  body: 'Go For Walk',
+  completed: false
+}, {
+  body: 'Take Shower',
+  completed: false
+}];
+
+// const findTodo = (todoList, todoBody) => {
+//   return todoList.find((todo, index) => {
+//     return todo.body.toLowerCase() === todoBody.toLowerCase();
+//   });
+// };
+
+// console.log(findTodo(todoList, 'Get Water'));
+
+const removeTodo = (todos, todoText) => {
+  const index = todos.findIndex((todo) => {
+    return todo.body.toLowerCase() === todoText.toLowerCase();
+  });
+  if(index > -1) {
+    todos.splice(index, 1)
+  }
+};
+
+removeTodo(todos, 'Get Coffee');
+console.log(todos);
+
+// Delete Get Coffee
+// Add new item to end
+// Remove first get coffee
+// console.log(`You have at least ${todoList.length} things to get done today. ${todoList[0]} and ${todoList[todoList.length - 1]} are pretty important`);
+// This starts at the third item, which is 2 in the array (Get Water), then removes it from the array. So 2 starts at Get Water, 1 removes Get Water
+// todoList.splice(2, 1);
+// Simply adding a thing to the end of the array.
+// todoList.push('Random Thing');
+// Removing first item from array
+// todoList.shift();
+// console.log(todoList);
+
+// todoList.forEach((todo, index) => {
+//   console.log(`${index+1} ${todo}`)
+// });
+
+// for(const value of Array.from(todoList)) {
+//   todoList.map((todo, index) => {
+//     console.log(`${index+1} ${todo}`);
+//   })
+// }
+
+// for(let count = todoList.length - 1; count >= 0; count--) {
+//   console.log(`This is your schedule for the day: ${todoList[count]}`);
+// }
+
+// for(let count = 0; count < todoList.length; count++) {
+//   console.log(`${count+1} ${todoList[count]}`)
+// }
