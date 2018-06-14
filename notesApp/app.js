e768a6625a7b9160bea133e7c886607313b656a0
elamahpla <theamazingness@gmail.com>
2018-06-14 18:03:44 -0500

    Stuff

--- notesApp/app.js
+++ notesApp/app.js
@@ -1,1 +1,7 @@
-console.log('Hi');
+// const paragraph = document.querySelector('p');
+// paragraph.remove();
+
+const paragraphs = document.querySelectorAll('p');
+paragraphs.forEach((p) => {
+  p.textContent[0] = 'fugyeah';
+});
