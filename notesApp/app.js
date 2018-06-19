ee47f6f05d014106a848300c3700f904bdfea6ac
elamahpla <theamazingness@gmail.com>
2018-06-19 11:08:02 -0500

    Updating

--- notesApp/app.js
+++ notesApp/app.js
@@ -9,49 +9,23 @@ const notes = [{
   body: 'Get another monitor'
 }];
 
-const filters = {
-  searchText: ''
-};
+// const paragraph = document.querySelector('p');
+// paragraph.remove();
+// Will target the first element that's given. Example: p for paragraph
+// h1 for h1, div for div, etc. This will target the first in the dom.
 
-const renderNotes = (notes, filters) => {
-  const filteredNotes = notes.filter((notes) => {
-    return notes.title.toLowerCase().includes(filters.searchText.toLowerCase());
-  });
-  document.getElementById('notes').innerHTML = '';
-  filteredNotes.forEach((note) => {
-    const noteElement = document.createElement('p');
-    noteElement.textContent = note.title;
-    document.getElementById('notes').appendChild(noteElement);
-  });
-};
-
-renderNotes(notes, filters);
-// const paragraphs = document.querySelectorAll('p');
-// paragraphs.forEach((p) => {
-//   p.textContent[0] = 'fugyeah';
-// });
-
-// const newParagraphs = document.createElement('p');
-// newParagraphs.textContent = 'New shit';
-// document.querySelector('body').appendChild(newParagraphs);
-
-const createNote = (event) => {
-  event.preventDefault();
-  console.log('Hi');
-}
-
-// const removeAllNotes = (event) => {
-//   event.preventDefault();
-//   console.log('Removed');
-// }
-document.getElementById('createNote').addEventListener('click', createNote);
-document.getElementById('removeAllNotes').addEventListener('click', (event) => {
-  document.querySelectorAll('.note').forEach((note) => {
-    note.remove();
-  });
+const paragraph = document.querySelectorAll('p');
+paragraph.forEach((paragraphs) => {
+  paragraphs.textContent = 'Hello';
+  // paragraphs.remove();
 });
+// Similar to above, expect this is all of a given tag.
 
-document.getElementById('searchNotes').addEventListener('input', (event) => {
-  filters.searchText = event.target.value;
-  renderNotes(notes, filters);
-});
+// Here, I'm creating a new element with createElement
+// Then, I'm setting the textContent = 'This is a new element'
+// After that, I'm using document.querySelector to select the body of the DOM
+// And then using appendChild(newParagraph) to append the variable to the body
+// Of the dom. So the result is the paragraph being added.
+const newParagraph = document.createElement('p');
+newParagraph.textContent = 'This is a new element';
+document.querySelector('body').appendChild(newParagraph);
--- todoApp/app.js
+++ todoApp/app.js
@@ -15,61 +15,36 @@ const todos = [{
   completed: false
 }];
 
-const filters = {
-  searchText: ''
-};
 
-const renderTodos = (todos, filters) => {
-  const filteredTodos = todos.filter((todo) => {
-    return todo.text.toLowerCase().includes(filters.searchText.toLowerCase());
-  });
-  const incompleteTodos = filteredTodos.filter((todo) => {
-    return !todo.completed;
-  });
-  document.getElementById('todos').innerHTML = '';
-  const summary = document.createElement('h3');
-  summary.textContent = `You have ${incompleteTodos.length} todost left`
-  document.getElementById('todos').appendChild(summary);
-  filteredTodos.forEach((todo) => {
-    const para = document.createElement('p');
-    para.textContent = todo.body;
-    document.getElementById('todos').appendChild(para);
-  });
-};
+// In this block, I'm filtering over the todos array and returning !todo.completed. I'm going to use this variable further down.
+const incompleteTodos = todos.filter((todo) => {
+  return !todo.completed;
+});
+// Here, I'm creating an h2 element to server as a staging area
+const summary = document.createElement('h2');
+// summary.textContent calls the incompleteTodos. Meaning that any todos.completed that are >FALSE< will be shown here
+summary.textContent = `You have ${incompleteTodos.length} todos left`;
+// Next, I'm again using querySelector to appendChild the summary variable to the body. This results in any todos that are
+// false, showing. In this case it would be this: You have 2 todos left.
+document.querySelector('body').appendChild(summary);
 
-// const createTodo = (event) => {
-//   console.log('hi');
-// }
-document.getElementById('createTodo').addEventListener('click', createTodo);
-
-document.getElementById('todoInput').addEventListener('input', (event) => {
-  console.log(event.target.value);
+// Here, I'm using forEach to iterate over the todos array. I'm making a new variable (paragraph) to create a new element
+// (p). After that, I'm using paragraph.textContent and setting that to the todo(s).body. Finally, I'm using the 
+// document.querySelector to target the <body> element, then using appendChild to add the paragraph variable to the DOM
+// This results in all the todos printing to the screen.
+todos.forEach((todo) => {
+  const paragraph = document.createElement('p');
+  paragraph.textContent = todo.body
+  document.querySelector('body').appendChild(paragraph);
 });
 
-// const incompleteTodos = todos.filter((todo) => {
-//   return !todo.completed;
-// });
 
-// const summary = document.createElement('h3');
-// summary.textContent = `You have ${incompleteTodos.length} todost left`
-// document.querySelector('body').appendChild(summary);
-
-// todos.forEach((todo) => {
-//   const para = document.createElement('p');
-//   para.textContent = todo.body;
-//   document.querySelector('body').appendChild(para);
-// });
-
-document.getElementById('searchTodos').addEventListener('input', (event) => {
-  filter.searchText = event.target.value;
-});
-
-// const paragraphs = document.querySelectorAll('p');
-// paragraphs.forEach((paragraph) => {
-  
-// })
-// paragraphs.forEach((paragraph) => {
-//   if(paragraph.textContent.includes('The')) {
-//     paragraph.textContent = '';
+// Here I'm using forEach() to go over the paragraphs in the HTML document
+// I'm looking for the textContent. of each paragraph to see if any of them contain
+// The word 'the'. If it does, I call .remove(); on it, removing that form the DOM.
+// const paragraph = document.querySelectorAll('p');
+// paragraph.forEach((paragraphs) => {
+//   if(paragraphs.textContent.includes('the')) {
+//     paragraphs.remove();
 //   }
 // });
