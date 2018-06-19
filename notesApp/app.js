2a05fb72aade33383d62aadefdccb87753147022
elamahpla <theamazingness@gmail.com>
2018-06-19 09:36:47 -0500

    Updating from yesterday. I'm so bored

--- notesApp/app.js
+++ notesApp/app.js
@@ -9,6 +9,23 @@ const notes = [{
   body: 'Get another monitor'
 }];
 
+const filters = {
+  searchText: ''
+};
+
+const renderNotes = (notes, filters) => {
+  const filteredNotes = notes.filter((notes) => {
+    return notes.title.toLowerCase().includes(filters.searchText.toLowerCase());
+  });
+  document.getElementById('notes').innerHTML = '';
+  filteredNotes.forEach((note) => {
+    const noteElement = document.createElement('p');
+    noteElement.textContent = note.title;
+    document.getElementById('notes').appendChild(noteElement);
+  });
+};
+
+renderNotes(notes, filters);
 // const paragraphs = document.querySelectorAll('p');
 // paragraphs.forEach((p) => {
 //   p.textContent[0] = 'fugyeah';
@@ -22,4 +39,19 @@ const createNote = (event) => {
   event.preventDefault();
   console.log('Hi');
 }
+
+// const removeAllNotes = (event) => {
+//   event.preventDefault();
+//   console.log('Removed');
+// }
 document.getElementById('createNote').addEventListener('click', createNote);
+document.getElementById('removeAllNotes').addEventListener('click', (event) => {
+  document.querySelectorAll('.note').forEach((note) => {
+    note.remove();
+  });
+});
+
+document.getElementById('searchNotes').addEventListener('input', (event) => {
+  filters.searchText = event.target.value;
+  renderNotes(notes, filters);
+});
--- todoApp/app.js
+++ todoApp/app.js
@@ -15,10 +15,36 @@ const todos = [{
   completed: false
 }];
 
-const createTodo = (event) => {
-  console.log('hi');
-}
+const filters = {
+  searchText: ''
+};
+
+const renderTodos = (todos, filters) => {
+  const filteredTodos = todos.filter((todo) => {
+    return todo.text.toLowerCase().includes(filters.searchText.toLowerCase());
+  });
+  const incompleteTodos = filteredTodos.filter((todo) => {
+    return !todo.completed;
+  });
+  document.getElementById('todos').innerHTML = '';
+  const summary = document.createElement('h3');
+  summary.textContent = `You have ${incompleteTodos.length} todost left`
+  document.getElementById('todos').appendChild(summary);
+  filteredTodos.forEach((todo) => {
+    const para = document.createElement('p');
+    para.textContent = todo.body;
+    document.getElementById('todos').appendChild(para);
+  });
+};
+
+// const createTodo = (event) => {
+//   console.log('hi');
+// }
 document.getElementById('createTodo').addEventListener('click', createTodo);
+
+document.getElementById('todoInput').addEventListener('input', (event) => {
+  console.log(event.target.value);
+});
 
 // const incompleteTodos = todos.filter((todo) => {
 //   return !todo.completed;
@@ -33,6 +59,10 @@ document.getElementById('createTodo').addEventListener('click', createTodo);
 //   para.textContent = todo.body;
 //   document.querySelector('body').appendChild(para);
 // });
+
+document.getElementById('searchTodos').addEventListener('input', (event) => {
+  filter.searchText = event.target.value;
+});
 
 // const paragraphs = document.querySelectorAll('p');
 // paragraphs.forEach((paragraph) => {
